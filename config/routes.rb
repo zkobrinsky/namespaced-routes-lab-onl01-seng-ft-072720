534d5755f350e0fb2c5cde9ae713d217841f6d72
Scott C. Reynolds <scott@scottcreynolds.com>
2016-03-10 12:15:27 -0500

    update solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :artists do
+    resources :songs, only: [:index, :show]
+  end
+  resources :songs
+
+  namespace :admin do
+    resources :preferences, only: [:index, :update]
+  end
 end
