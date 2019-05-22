b0bae6b3fb0b3c76d6aaddea777232cd16003757
Steven G. Harms <steven.harms@flatironschool.com>
2019-05-21 20:14:03 -0400

    Revert to non-pref aware controllers

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,18 +1,12 @@
 class SongsController < ApplicationController
-  before_action :set_preferences, only: [:index, :new]
-
   def index
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found"
-      elsif @preferences && @preferences.song_sort_order
-        @songs = @artist.songs.order(title: @preferences.song_sort_order)
       else
         @songs = @artist.songs
       end
-    elsif @preferences && @preferences.song_sort_order
-      @songs = Song.order(title: @preferences.song_sort_order)
     else
       @songs = Song.all
     end
@@ -31,11 +25,7 @@ class SongsController < ApplicationController
   end
 
   def new
-    if @preferences && !@preferences.allow_create_songs
-      redirect_to songs_path
-    else
-      @song = Song.new
-    end
+    @song = Song.new
   end
 
   def create
@@ -76,8 +66,5 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title, :artist_name)
   end
-
-  def set_preferences
-    @preferences = Preference.first
-  end
 end
+
