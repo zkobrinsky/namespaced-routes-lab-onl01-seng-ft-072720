4a5c5544de536d6ac3b0bf6b98c3cec001c30a7d
Mohawk Greene <kellyecodes@gmail.com>
2019-11-01 16:46:04 -0400

    Update controllers

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,6 @@
 class ArtistsController < ApplicationController
-  before_action :set_preferences, only: [:index, :new]
-
   def index
-    if @preferences && @preferences.artist_sort_order
-      @artists = Artist.order(name: @preferences.artist_sort_order)
-    else
-      @artists = Artist.all
-    end
+    @artists = Artist.all
   end
 
   def show
@@ -14,10 +8,11 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    if @preferences && !@preferences.allow_create_artists
-      redirect_to artists_path
+    @preference = Preference.first
+    if @preference.allow_create_artists
+       @artist = Artist.new
     else
-      @artist = Artist.new
+      redirect_to artists_path
     end
   end
 
@@ -58,9 +53,5 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
-  end
-
-  def set_preferences
-    @preferences = Preference.first
   end
 end
